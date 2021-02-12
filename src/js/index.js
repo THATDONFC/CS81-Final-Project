a3a8aab392b9a703cb3d3e3e1479f1bb61f74769
thatdonfc <thatdonfc+git@gmail.com>
2021-02-11 16:05:12 -0800

    Move index.html to Bootstrap
    # Add average and maximum power values
    # Add recommended power supply
    # Formatting html

--- src/js/index.js
+++ src/js/index.js
@@ -36,3 +36,85 @@ function setV() {
 // class texting {
 
 // }
+
+// function checkForCharacters(inputString, checkString, startingIndex) {
+
+//   if (!startingIndex) {
+// 	  startingIndex = 0;
+//   }
+//   return inputString.indexOf(checkString);
+// }
+
+// function isNotaNumber (inputString) {
+//   return isNaN(inputString);
+// }
+
+// function isNumberInt(inputString) {
+
+//   return (!isNaN(parseInt(inputString))) ? true : false;
+// }
+
+// function stringToLowercase(inputString) {
+//   return inputString.toLowerCase();
+// }
+
+// function maxLength(inputString, inputLength) {
+
+//   return (inputString.length <= inputLength) ? true : false;
+// }
+
+// function minLength(inputString,inputLength) {
+//   return (inputString.length >= inputLength) ? true : false;
+// }
+
+// function stringToInteger (inputString) {
+//   return parseInt(inputString);
+// }
+
+// /* Returns viewport's width */
+// function getViewportWidth() {
+
+//     if (window.innerWidth) {
+//   /* Returns Window's width excluding toolbars/scrollbars */
+//         return window.innerWidth;
+//     } else if (document.body && document.body.offsetWidth) {
+//   /* Returns viewable width of document,
+//   including padding, border and scrollbar, but not the margin */
+//         return document.body.offsetWidth;
+//     } else {
+//         return 0;
+//     }
+// }
+
+// /* Returns window's height */
+// function getViewportHeight() {
+
+//     if (window.innerHeight) {
+//   /* Returns Window's height excluding toolbars/scrollbars */
+//         return window.innerHeight;
+//     } else if (document.body && document.body.offsetHeight) {
+//   /* Returns viewable height of document,
+//   including padding, border and scrollbar, but not the margin */
+//         return document.body.offsetHeight;
+//     } else {
+//         return 0;
+//     }
+// }
+
+// /* Returns screen's total height & width */
+// function getScreenResolution() {
+//  return {
+//   height: (screen.height)? screen.height : undefined,
+//   width: (screen.width)? screen.width : undefined
+//  };
+// }
+
+// /* Returns coordinates of the mouse pointer relative to the current window, for the given mouse event.
+//    Example, onclick="coords = getMouseCoordinates(event)" or onmousemove="coords = getMouseCoordinates(event)"
+// */
+// function getMouseCoordinates(event) {
+//     var x = (event.clientX)? event.clientX : undefined;
+//     var y = (event.clientY)? event.clientY : undefined;
+
+//  return { x: x, y: y };
+// }
